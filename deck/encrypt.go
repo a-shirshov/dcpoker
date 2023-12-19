7f652ce9de203773f0ef331d4054f912c5e9622e
a-shirshov <artyomsh01@yandex.ru>
2023-12-19 15:02:45 +0300

    EP 7 done

--- deck/encrypt.go
+++ deck/encrypt.go
@@ -5,10 +5,35 @@ import (
 	"encoding/gob"
 )
 
-func encrypt(c Card) error {
-	buf := new(bytes.Buffer)
-	if err := gob.NewEncoder(buf).Encode(c); err != nil {
-		return err
+func DecryptCard(key, encCard []byte) (Card, error) {
+	card := Card{}
+
+	decCard, err := Encrypt(key, encCard)
+	if err != nil {
+		return card, nil
 	}
-	return nil
+
+	if err := gob.NewDecoder(bytes.NewReader(decCard)).Decode(&card); err != nil {
+		return card, err
+	}
+	return card, nil
+}
+
+func EncryptCard(key []byte, card Card) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	if err := gob.NewEncoder(buf).Encode(card); err != nil {
+		return nil, err
+	}
+
+	payload := buf.Bytes()
+	return Encrypt(key, payload)
+}
+
+func Encrypt(key, payload []byte) ([]byte, error) {
+	encOutput := make([]byte, len(payload))
+	for i := 0; i < len(payload); i++ {
+		encOutput[i] = payload[i] ^ key[i%len(key)]
+	}
+
+	return encOutput, nil
 }
--- deck/encrypt_test.go
+++ deck/encrypt_test.go
@@ -1,7 +1,27 @@
 package deck
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestEncryptCard(t *testing.T) {
+	key := []byte("foobarbazfoobarbazfoobarbazfoobarbazfoobarbaz")
+	card := Card{
+		Suit: Spades,
+		Value: 1,
+	}
 
+	encOutput, err := EncryptCard(key, card)
+	if err != nil {
+		t.Error(err)
+	}
+
+	decCard, err := DecryptCard(key, encOutput)
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Printf("%b\n", encOutput)
+	fmt.Printf("%+v\n", decCard)
 }
